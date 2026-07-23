e77c38591896ef691c691721084bb3aa57a99fc4
Frol3z <g.novello1006@gmail.com>
2026-07-23 13:16:19 +0200

    Non me lo ricordo

---
+++ script/main.gd
@@ -0,0 +1,8 @@
+extends Node2D
+
+# GAMEPLAY LOOP
+# Spawna nemico.
+# Il timer/countdown parte e hai tot secondi per svolgere 
+# quante più azioni possibili con il mecha.
+# Una volta terminato il countdown il nemico attacca e i danni vengono calcolati.
+# Ripeti con diversi tipi di nemici.
--- script/menu.gd
+++ script/menu.gd
@@ -4,3 +4,15 @@ const MAIN_SCENE = preload("res://main.tscn");
 
 func _on_play_button_pressed() -> void:
 	get_tree().change_scene_to_packed(MAIN_SCENE);
+
+
+func _on_settings_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_credits_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_exit_pressed() -> void:
+	get_tree().quit()
